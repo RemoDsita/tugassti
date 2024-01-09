e12f9e6aa9c4d98ec6d631971396baa1e6e8d59f
RemoDsita <149335190+RemoDsita@users.noreply.github.com>
2024-01-09 11:41:52 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// togle & renponsive navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navLists = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    // toggle nav list and burger class
+    navLists.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
+// clear form before unload
+window.onbeforeunload = () => {
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
